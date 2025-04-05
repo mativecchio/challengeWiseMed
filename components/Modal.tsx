f08f12228b4d7a1985634c0e32151e1cb160fbf0
Matias Vecchio <mvecchio@prosite.soccer>
2025-04-05 19:15:09 -0300

    modal shadow

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -12,5 +12,14 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderWidth: 1,
     borderColor: "#719EC0",
+    shadowOffset: {
+      height: -5,
+      width: 0,
+    },
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
